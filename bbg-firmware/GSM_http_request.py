e45bf986e71a6a16ca1801c1f11c57267ec70e6e
crashtack <crashtack@gmail.com>
2016-10-11 11:37:28 -0700

    Broke up into functions Added multiple commands

--- bbg-firmware/GSM_http_request.py
+++ bbg-firmware/GSM_http_request.py
@@ -5,7 +5,7 @@ from time import sleep
 def setup_serial():
     """ Setup serial connection """
     ser = Serial(port="/dev/ttyO2", baudrate=9600, timeout=0)
-    ser.close(0)
+    ser.close()
     sleep(0.1)
     ser.open()
     sleep(0.1)
@@ -41,5 +41,25 @@ def close_serial(ser):
 if __name__ == "__main__":
 
     ser = setup_serial()
-    print(send_command(ser, 'AT+CBC'))
+    commands = []
+    commands.append(b'AT')
+    commands.append(b'AT+CBC')
+    commands.append(b'AT+SAPBR=3,1,"Contype","GPRS"')
+    commands.append(b'AT+SAPBR=3,1,"APN","www"')
+    commands.append(b'AT+SAPBR=1,1')
+    commands.append(b'AT+SAPBR=2,1')
+    commands.append(b'AT+HTTPINIT')
+    commands.append(b'AT+HTTPPARA="URL","http://ec2-52-33-25-11.us-west-2.compute.amazonaws.com/"')
+    commands.append(b'AT+HTTPACTION=0')
+    commands.append(b'AT+HTTPREAD')
+    commands.append(b'AT+HTTPREAD')
+
+    for com in commands:
+        response = send_command(ser, com)
+        print(response)
+        if 'ERROR' in response:
+            response = send_command(ser, com)
+            print(response)
+    # print(send_command(ser, b'AT+CBC'))
+    # print(send_command(ser, ''))
     close_serial(ser)

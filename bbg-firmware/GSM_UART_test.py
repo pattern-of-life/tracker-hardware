ae8f10803d64b911f0fea10af82696c7c4d70c23
crashtack <crashtack@gmail.com>
2016-10-11 09:26:57 -0700

    Successfull GSM send command and receive reply Fixes pattern-of-life/tracker#33

--- bbg-firmware/GSM_UART_test.py
+++ bbg-firmware/GSM_UART_test.py
@@ -15,16 +15,18 @@ data = ''
 
 if ser.isOpen():
     print "Serial is open!"
-    print('Bytes sent: {}'.format(ser.write(b'AT')))
-
+    print('Bytes sent: {}'.format(ser.write(b'AT+CBC\n')))
+    time.sleep(0.2)
     bytes_read = 0
     while True:
         data = ser.readline()
         bytes_read += 1
         response += data
+        time.sleep(0.1)
         if data == '':
             break
-
+    else:
+        print('serial connection was not open')
     print(response)
 
 ser.close()

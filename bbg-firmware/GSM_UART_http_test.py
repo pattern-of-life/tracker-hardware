5b8f1c975fef98dba1e7c38f20897999b84fa7f7
crashtack <crashtack@gmail.com>
2016-10-10 18:23:16 -0700

    Still trying to get the AT responses back correctly

--- bbg-firmware/GSM_UART_http_test.py
+++ bbg-firmware/GSM_UART_http_test.py
@@ -5,47 +5,41 @@ import time
 # UART.setup("UART2")
 
 
-def serial_setup():
+def send_commands(commands):
     """opens a serial connection and returns it"""
     ser = serial.Serial(port="/dev/ttyO2", baudrate=9600, timeout=2)
     ser.close()
     time.sleep(0.1)
     ser.open()
     time.sleep(0.1)
-    return ser
 
+    for com in commands:
+        response = ''
+        data = ''
 
-def serial_close(ser):
-    """close serial connection"""
+        if ser.isOpen():
+            print('Bytes sent: {}'.format(ser.write(com)))
+            # print(ser.write(com))
+            bytes_read = 0
+            while True:
+                data = ser.readline()
+                bytes_read += 1
+                response += data
+                if data == '':
+                    break
+            print("Response: {}".format(response))
+        else:
+            print('serial connection was not open')
+
     ser.close()
 
 
-def send_command(ser, com):
-    """send a AT command and return the response"""
-    response = ''
-    data = ''
-
-    if ser.isOpen():
-        print('Bytes sent: {}'.format(ser.write(com)))
-        print(ser.write(com))
-        bytes_read = 0
-        while True:
-            data = ser.readline()
-            bytes_read += 1
-            response += data
-            if data == '':
-                break
-    else:
-        print('serial connection was not open')
-    return response
-
-
-ser = serial_setup()
-
-print(send_command(ser, b'AT'))
-print(send_command(ser, b'AT'))
-print(send_command(ser, b'AT'))
-print(send_command(ser, b'AT+SAPBR=3,1,"Contype","GPRS"'))
-print(send_command(ser, b'AT+SAPBR=3,1,"APN","www"'))
-print(send_command(ser, b'AT+SAPBR=1,1'))
-print(send_command(ser, b'AT+SAPBR=2,1'))
+commands = []
+commands.append(b'AT')
+commands.append(b'AT')
+commands.append(b'AT')
+commands.append(b'AT+SAPBR=3,1,"Contype","GPRS"')
+commands.append(b'AT+SAPBR=3,1,"APN","www"')
+commands.append(b'AT+SAPBR=1,1')
+commands.append(b'AT+SAPBR=2,1')
+send_commands(commands)
